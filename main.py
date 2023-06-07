41381c52756fa79605090f2915ab26109f623214
musser004 <113221988+musser004@users.noreply.github.com>
2023-06-07 00:59:39 -0400

    Update main and requirements.txt for deployment
    - Updating main.py to include MySQLdb import
    - Updated requirements.txt to include several other libraries that may help with deploy log errors on railway

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
 from functools import wraps
 import os
+import MySQLdb
 
 # Initial Flask setup
 
